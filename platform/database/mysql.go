af491d3fe9fd92f7708518d97d92110edd3ab65d
Eeap <kdg97811@naver.com>
2023-07-02 11:31:09 +0900

    init: proj init settings

---
+++ platform/database/mysql.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/create-go-app/fiber-go-template/pkg/utils"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/go-sql-driver/mysql" // load driver for Mysql
+)
+
+// MysqlConnection func for connection to Mysql database.
+func MysqlConnection() (*sqlx.DB, error) {
+	// Define database connection settings.
+	maxConn, _ := strconv.Atoi(os.Getenv("DB_MAX_CONNECTIONS"))
+	maxIdleConn, _ := strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNECTIONS"))
+	maxLifetimeConn, _ := strconv.Atoi(os.Getenv("DB_MAX_LIFETIME_CONNECTIONS"))
+
+	// Build Mysql connection URL.
+	mysqlConnURL, err := utils.ConnectionURLBuilder("mysql")
+	if err != nil {
+		return nil, err
+	}
+
+	// Define database connection for Mysql.
+	db, err := sqlx.Connect("mysql", mysqlConnURL)
+	if err != nil {
+		return nil, fmt.Errorf("error, not connected to database, %w", err)
+	}
+
+	// Set database connection settings:
+	// 	- SetMaxOpenConns: the default is 0 (unlimited)
+	// 	- SetMaxIdleConns: defaultMaxIdleConns = 2
+	// 	- SetConnMaxLifetime: 0, connections are reused forever
+	// setConnMax부분에서 min으로 maxlife를 길게 설정해주지 않으면 conn이 날라가는 것 같음!
+	db.SetMaxOpenConns(maxConn)
+	db.SetMaxIdleConns(maxIdleConn)
+	db.SetConnMaxLifetime(time.Duration(maxLifetimeConn) * time.Minute)
+
+	// Try to ping database.
+	if err := db.Ping(); err != nil {
+		defer db.Close() // close database connection
+		return nil, fmt.Errorf("error, not sent ping to database, %w", err)
+	}
+
+	return db, nil
+}
