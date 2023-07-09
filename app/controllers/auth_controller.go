da5b8fec085cb5ca79edb4b0fcc9d58b34a9d214
Eeap <kdg97811@naver.com>
2023-07-09 11:06:30 +0900

    refactor: add check user with db user

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -4,17 +4,37 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"log"
 	"main/app/models"
+	"main/app/queries"
+	"main/platform/database"
 )
 
 // UserSignIn method to auth user and return access and refresh tokens.
 // @Router /v1/user/sign/in [post]
 func UserSignIn(c *fiber.Ctx) error {
 	// Return status 200 OK.
-	user := new(models.User)
+	signIn := &models.SignIn{}
 
-	if err := c.BodyParser(user); err != nil {
-		log.Println(user)
+	if err := c.BodyParser(signIn); err != nil {
+		log.Println(signIn)
 		return c.Status(fiber.StatusBadRequest).JSON(err)
+	}
+	db, err := database.OCINoSQLConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err)
+	}
+	userQueries := queries.UserQueries{Client: db}
+	user, err := userQueries.GetUserByEmail(signIn.Email)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   "user with the given email is not found",
+		})
+	}
+	if user.Password != signIn.Password {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   "user with the given password is not correct",
+		})
 	}
 	return c.JSON(fiber.Map{
 		"authInfo": map[string]string{
