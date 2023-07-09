e42805aa084f020e234cde3cf9addf939c43fe83
Eeap <kdg97811@naver.com>
2023-07-09 09:50:25 +0900

    feat: add login handler api

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -2,34 +2,29 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"log"
+	"main/app/models"
 )
 
 // UserSignIn method to auth user and return access and refresh tokens.
-// @Description Auth user and return access and refresh token.
-// @Summary auth user and return access and refresh token
-// @Tags User
-// @Accept json
-// @Produce json
-// @Param email body string true "User Email"
-// @Param password body string true "User Password"
-// @Success 200 {string} status "ok"
 // @Router /v1/user/sign/in [post]
 func UserSignIn(c *fiber.Ctx) error {
 	// Return status 200 OK.
+	user := new(models.User)
+
+	if err := c.BodyParser(user); err != nil {
+		log.Println(user)
+		return c.Status(fiber.StatusBadRequest).JSON(err)
+	}
 	return c.JSON(fiber.Map{
-		"error": false,
-		"msg":   nil,
+		"authInfo": map[string]string{
+			"accessToken":  "testAccess",
+			"refreshToken": "testRefresh",
+		},
 	})
 }
 
 // UserSignOut method to de-authorize user and delete refresh token from Redis.
-// @Description De-authorize user and delete refresh token from Redis.
-// @Summary de-authorize user and delete refresh token from Redis
-// @Tags User
-// @Accept json
-// @Produce json
-// @Success 204 {string} status "ok"
-// @Security ApiKeyAuth
 // @Router /v1/user/sign/out [post]
 func UserSignOut(c *fiber.Ctx) error {
 	// Return status 204 no content.
--- app/models/user_model.go
+++ app/models/user_model.go
@@ -2,9 +2,6 @@ package models
 
 // User struct to describe User object.
 type User struct {
-	ID           int    `json:"id" validate:"required,unique"`
-	Email        string `json:"email" validate:"required,email,lte=255"`
-	PasswordHash string `json:"password_hash" validate:"required,lte=255"`
-	AccessKey    string `json:"accessKey" validate:"required,lte=255"`
-	SecretKey    string `json:"secretKey" validate:"required,lte=255"`
+	Email    string `json:"email" validate:"required,email,lte=255"`
+	Password string `json:"password" validate:"required,lte=255"`
 }
