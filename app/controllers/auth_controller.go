cb08e856f1c571e6b588cd5f90f048f288205a11
Eeap <kdg97811@naver.com>
2023-07-13 00:43:30 +0900

    feat: add user key save api

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -1,7 +1,14 @@
 package controllers
 
 import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"log"
+	"os"
+	"strings"
+
+	"main/platform/OCI"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -56,4 +63,74 @@ func UserSignIn(c *fiber.Ctx) error {
 func UserSignOut(c *fiber.Ctx) error {
 	// Return status 204 no content.
 	return c.SendStatus(fiber.StatusNoContent)
+}
+
+// UserKeySave는 key를 bucket에 업로드하고 env로 해당 키를 등록하는 함수
+// @Router /v1/user/key [post]
+func UserKeySave(c *fiber.Ctx) error {
+	userKey := &models.UserKey{}
+	if err := c.BodyParser(userKey); err != nil {
+		log.Println(userKey)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+	filePath := fmt.Sprintf("./%s.csv", userKey.Email)
+	createFile(filePath, userKey.AccessKey, userKey.SecretKey)
+	file, err := os.Open(filePath)
+	fi, err := file.Stat()
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+	err = OCI.PutObject("canvas-bucket", userKey.Email+".csv", fi.Size(), file, nil)
+	defer func() {
+		os.Remove(filePath)
+	}()
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+	result, err := OCI.GetObject("canvas-bucket", userKey.Email+".csv")
+	result = deleteWithTrim(result)
+	// 환경변수에 등록 (추후 변경이 필요한 부분 => 이 키를 사용하는 부분은 버킷에서 불러오는게 효율적인 방법일듯)
+	os.Setenv("AWS_ACCESS_KEY", result[0])
+	os.Setenv("AWS_SECRET_KEY", result[1])
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error": false,
+		"msg":   "userKeySave success",
+	})
+}
+
+func deleteWithTrim(result []string) []string {
+	for idx, key := range result {
+		result[idx] = strings.Trim(key, "\n")
+	}
+	return result
+}
+
+func createFile(filePath string, accessKey string, secretKey string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println(err)
+	}
+	// csv writer 생성
+	wr := csv.NewWriter(bufio.NewWriter(file))
+	// csv 내용 쓰기
+	wr.Write([]string{accessKey, secretKey})
+	wr.Flush()
 }
