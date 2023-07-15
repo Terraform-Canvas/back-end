e932506a5824e649e1d788c4f5b1d316390572bb
dusdjhyeon <dusdj0813@gmail.com>
2023-07-15 17:02:50 +0900

    refactor: merge api and add initialize folder

--- app/controllers/terraform_controller.go
+++ app/controllers/terraform_controller.go
@@ -1,17 +1,14 @@
 package controllers
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"path/filepath"
+
 	"main/app/services"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-// accept랑 produce는 (swagger에서의) 애매해서 생략(s3에서도 생략되어 있음)
-// Description Merge module tf file and Add in user folder
-// Summary merge module tf file and add in user folder
-// Tags Env
-// Param email path string true "user email"
-// Success 200 {string} status "ok"
-
+// Create a user env tf
 // @Router /v1/terraform/merge/{email} [post]
 func MergeEnvTf(c *fiber.Ctx) error {
 	var data []map[string]interface{}
@@ -23,8 +20,25 @@ func MergeEnvTf(c *fiber.Ctx) error {
 	}
 
 	email := c.Params("email")
+	userFolderPath := filepath.Join("usertf", email)
 
-	err := services.MergeEnvTf(email, data)
+	err := services.InitializeFolder(userFolderPath)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+
+	err = services.MergeEnvTf(userFolderPath, data)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   err,
+		})
+	}
+
+	err = services.CreateTfvars(userFolderPath, data)
 	if err != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error": true,
@@ -34,31 +48,11 @@ func MergeEnvTf(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"error": false,
+		"msg":   "merge user env tf success",
 	})
 }
 
-// @Router /v1/terraform/tfvars/{email} [post]
-func CreateTfvars(c *fiber.Ctx) error {
-	var data []map[string]interface{}
-	if err := c.BodyParser(&data); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(err)
-	}
-
-	email := c.Params("email")
-
-	err := services.CreateTfvars(email, data)
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": true,
-			"msg":   err,
-		})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"error": false,
-	})
-}
-
+// Apply user env's tf
 // @Router /v1/terraform/apply/{email} [post]
 func ApplyEnvTf(c *fiber.Ctx) error {
 	email := c.Params("email")
