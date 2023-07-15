19dd3f1b77b605f8b9bfcf8c68c503a7c164ea3a
dusdjhyeon <dusdj0813@gmail.com>
2023-07-15 17:19:17 +0900

    chore: update tf file

--- platform/terraform/alb/main.tf
+++ platform/terraform/alb/main.tf
@@ -1,41 +1,41 @@
 #alb 
 module "alb" {
-    source = "terraform-aws-modules/alb/aws"
-    version = "~> 8.0"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
 
-    name = "terramino"
-    
-    load_balancer_type = "application"
-    
-    vpc_id = module.vpc.vpd_id
-    subnets = module.vpc.public_subnets
-    security_groups = [module.terramino_lb.id]
-    internal = false
+  name = "terramino"
 
-    http_tcp_listeners=[
-        {
-            port = 80
-            protocol="HTTP"
-            action_type = "forward"
-        }
-    ]
+  load_balancer_type = "application"
 
-    target_groups = [
-        name = "learn-asg-terramino"
-        backend_port = 80
-        backend_protocol = "HTTP"
-    ]
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.terramino_lb.security_group_id]
+  internal        = false
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "forward"
+    }
+  ]
+
+  target_groups = [{
+    name             = "learn-asg-terramino"
+    backend_port     = 80
+    backend_protocol = "HTTP"
+  }]
 }
 resource "aws_autoscaling_attachment" "terramino" {
-  autoscaling_group_name = aws_autoscaling_group.terramino.id
-  alb_target_group_arn   = aws_lb_target_group.terramino.arn
+  autoscaling_group_name = module.asg.autoscaling_group_id
+  lb_target_group_arn    = module.alb.target_group_arns[0]
 }
 
 module "terramino_instance" {
-    source = "terraform-aws-modules/security-group/aws//modules/http-80"
-    
-    name = "learn-asg-terramino-instance"
-    vpc_id = module.vpc.vpc_id
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-    ingress_with_security_group_id = module.terramino_lb.id
+  name   = "learn-asg-terramino-instance"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_source_security_group_id = module.terramino_lb.security_group_id
 }
--- platform/terraform/asg/variables.tf
+++ platform/terraform/asg/variables.tf
@@ -1,19 +1,19 @@
-variable asg_min_size {
-    type="number"
+variable "asg_min_size" {
+  type = number
 }
 
-variable asg_max_size {
-    type="number"
+variable "asg_max_size" {
+  type = number
 }
 
-variable asg_instance_type {
-    type="string"
+variable "asg_instance_type" {
+  type = string
 }
 
-variable asg_desired_capacity {
-    type="number"
+variable "asg_desired_capacity" {
+  type = number
 }
 
-variable asg_image_id {
-    type="string"
+variable "asg_image_id" {
+  type = string
 }
