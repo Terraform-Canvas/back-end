3015950deb732c7a810e88c7c643fadf6745f8cb
dusdjhyeon <dusdj0813@gmail.com>
2023-07-21 14:56:03 +0900

    fix: parse jwt token

--- pkg/utils/jwt_generator.go
+++ pkg/utils/jwt_generator.go
@@ -19,9 +19,9 @@ type Tokens struct {
 }
 
 // GenerateNewTokens func for generate a new Access & Refresh tokens.
-func GenerateNewTokens(id string, credentials []string) (*Tokens, error) {
+func GenerateNewTokens(email string) (*Tokens, error) {
 	// Generate JWT Access token.
-	accessToken, err := generateNewAccessToken(id, credentials)
+	accessToken, err := generateNewAccessToken(email)
 	if err != nil {
 		// Return token generation error.
 		return nil, err
@@ -40,7 +40,7 @@ func GenerateNewTokens(id string, credentials []string) (*Tokens, error) {
 	}, nil
 }
 
-func generateNewAccessToken(id string, credentials []string) (string, error) {
+func generateNewAccessToken(email string) (string, error) {
 	// Set secret key from .env file.
 	secret := os.Getenv("JWT_SECRET_KEY")
 
@@ -51,13 +51,8 @@ func generateNewAccessToken(id string, credentials []string) (string, error) {
 	claims := jwt.MapClaims{}
 
 	// Set public claims:
-	claims["id"] = id
+	claims["email"] = email
 	claims["expires"] = time.Now().Add(time.Minute * time.Duration(minutesCount)).Unix()
-
-	// Set private token credentials:
-	for _, credential := range credentials {
-		claims[credential] = true
-	}
 
 	// Create a new JWT access token with claims.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
