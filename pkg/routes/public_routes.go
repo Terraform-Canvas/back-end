54f4c5574b30ba0a48c75175edfa1d549c12a3b4
Eeap <kdg97811@naver.com>
2023-07-23 22:00:12 +0900

    refactor: add token expire check & divide route

--- pkg/routes/public_routes.go
+++ pkg/routes/public_routes.go
@@ -11,11 +11,6 @@ func PublicRoutes(a *fiber.App) {
 	route := a.Group("/api/v1")
 	// Routes for POST method:
 	route.Post("/login/new", controllers.UserSignIn)
-	route.Post("/terraform/usertf", controllers.MergeEnvTf)
-	route.Post("/terraform/apply", controllers.ApplyEnvTf)
-	route.Post("/s3/upload", controllers.UploadHandler)
-	route.Get("/s3/download", controllers.DownloadHandler)
-	route.Post("/user/key", controllers.UserKeySave)
 	route.Get("/ec2/instanceTypes", controllers.EC2InstanceTypes)
 	route.Get("/ec2/ami", controllers.EC2Images)
 }
