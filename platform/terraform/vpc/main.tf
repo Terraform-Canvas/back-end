f0ac12e9232c06cdcc048e2719a73d6ac0f9485c
dusdjhyeon <dusdj0813@gmail.com>
2023-07-27 11:33:50 +0900

    refactor: change zones and add versions

--- platform/terraform/vpc/main.tf
+++ platform/terraform/vpc/main.tf
@@ -1,13 +1,4 @@
 #vpc
-data "aws_availability_zones" "available" {
-  state = "available"
-
-  filter {
-    name   = "zone-name"
-    values = ["ap-northeast-2a", "ap-northeast-2c"]
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.1"
@@ -15,7 +6,7 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = var.vpc_azs
   private_subnets      = var.vpc_privatesubnet
   public_subnets       = var.vpc_publicsubnet
   enable_dns_hostnames = true
--- platform/terraform/vpc/variables.tf
+++ platform/terraform/vpc/variables.tf
@@ -2,6 +2,10 @@ variable "vpc_name" {
   type = string
 }
 
+variable "vpc_azs" {
+  type = list(any)
+}
+
 variable "vpc_cidr" {
   type = string
 }
