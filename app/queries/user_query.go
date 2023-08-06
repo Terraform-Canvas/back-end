003031813aed252ad642987c8a7710066da0e616
Eeap <kdg97811@naver.com>
2023-08-07 00:04:00 +0900

    refactor: fix nosql sdk config & method

--- app/queries/user_query.go
+++ app/queries/user_query.go
@@ -1,50 +1,63 @@
 package queries
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/oracle/nosql-go-sdk/nosqldb"
-	"github.com/oracle/nosql-go-sdk/nosqldb/jsonutil"
-	"github.com/oracle/nosql-go-sdk/nosqldb/types"
+	"github.com/oracle/oci-go-sdk/nosql"
 
 	"main/app/models"
 )
 
 type UserQueries struct {
-	*nosqldb.Client
+	nosql.NosqlClient
 }
 
 func (q *UserQueries) GetUserByEmail(email string) (models.User, error) {
-	key := types.ToMapValue("email", email)
+	key := fmt.Sprintf("email:%v", email)
+	tableName := "userTable"
+	compartmentId := os.Getenv("compartmentID")
 	user := models.User{}
-	getReq := &nosqldb.GetRequest{
-		TableName: "userTable",
-		Key:       key,
+	getReq := nosql.GetRowRequest{
+		TableNameOrId: &tableName,
+		Key:           []string{key},
+		CompartmentId: &compartmentId,
 	}
-	getRes, err := q.Get(getReq)
+	getRes, err := q.GetRow(context.TODO(), getReq)
 	if err != nil {
 		log.Println(err)
 		return user, err
 	}
-	if getRes.RowExists() {
-		data, err := jsonutil.ToObject(getRes.ValueAsJSON())
-		log.Println(getRes.ValueAsJSON())
-		if err != nil {
-			log.Println(err)
-			return user, err
-		}
-		user.Name = fmt.Sprintf("%v", data["name"])
-		user.Email = fmt.Sprintf("%v", data["email"])
-		user.Password = fmt.Sprintf("%v", data["password"])
-		user.RefreshToken = fmt.Sprintf("%v", data["refreshToken"])
-		return user, nil
+	data := getRes.Value
+	user.Name = fmt.Sprintf("%v", data["name"])
+	user.Email = fmt.Sprintf("%v", data["email"])
+	user.Password = fmt.Sprintf("%v", data["password"])
+	user.RefreshToken = fmt.Sprintf("%v", data["refreshToken"])
+	return user, nil
+}
 
+func (q *UserQueries) UpdateUser(user models.User) error {
+	mapValues := map[string]interface{}{
+		"email":        user.Email,
+		"password":     user.Password,
+		"name":         user.Name,
+		"refreshToken": user.RefreshToken,
 	}
-	log.Println("The row doesn't exist.")
-	return user, &fiber.Error{
-		Code:    404,
-		Message: "The data doesn't exist",
+	compartmentId := os.Getenv("compartmentID")
+	tableName := "userTable"
+	putReq := nosql.UpdateRowRequest{
+		TableNameOrId: &tableName,
+		UpdateRowDetails: nosql.UpdateRowDetails{
+			Value:         mapValues,
+			CompartmentId: &compartmentId,
+		},
 	}
+
+	_, err := q.UpdateRow(context.TODO(), putReq)
+	if err != nil {
+		return err
+	}
+	return nil
 }
